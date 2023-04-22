0d749bf79564cf1c9c369021f71aeb5351e67e2a
mariafernandamurillo <fernanda.murillo@uabc.edu.mx>
2023-04-21 21:49:55 -0700

    Assignment 1

--- app/routes.py
+++ app/routes.py
@@ -5,12 +5,12 @@ app = Flask(__name__)                       # create an instance of Flask into o
 
 @app.get("/")                               # we can now user or object's method "get" as a decorator.
 def index():                                # a decorator wraps a function (more on this in a bit)
-    me = {                                  # create a new dictionary with key/value pairs.
+    fer = {                                  # create a new dictionary with key/value pairs.
         "first_name": "Fernanda",           
         "last_name": "Murillo",
         "hobbies": "Something",
         "is_active": True
     }
 
-    return me                               # when  you return a dictionary from a view function,
+    return fer                               # when  you return a dictionary from a view function,
                                             # flask automatically converts in to JSON fon compatibility. 
